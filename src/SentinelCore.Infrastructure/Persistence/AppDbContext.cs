a2d73ac8836d4f8a5a4e98d3f1c8b9ceebc2aaad
Mohammad Hossein <hossinarab727@gmail.com>
2025-11-07 15:04:36 +0330

    Add DbContext

---
+++ src/SentinelCore.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,59 @@
+using System.Reflection;
+using SentinelCore.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using SentinelCore.Core.Entities.User;
+using SentinelCore.Core.Entities.AuditLog;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace SentinelCore.Infrastructure.Persistence;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    // DbSets
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Role> Roles => Set<Role>();
+    public DbSet<UserRole> UserRoles => Set<UserRole>();
+    public DbSet<AuditLog> AuditLogs => Set<AuditLog>();
+    public DbSet<UserLogin> UserLogins => Set<UserLogin>();
+    public DbSet<Permission> Permissions => Set<Permission>();
+    public DbSet<UserPassword> UserPasswords => Set<UserPassword>();
+    public DbSet<RolePermission> RolePermissions => Set<RolePermission>();
+    public DbSet<SmsVerificationCode> SmsVerificationCodes => Set<SmsVerificationCode>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            if (typeof(BaseEntity).IsAssignableFrom(entityType.ClrType))
+            {
+                var method = typeof(AppDbContext)
+                    .GetMethod(nameof(SetSoftDeleteFilter), BindingFlags.NonPublic | BindingFlags.Static)!
+                    .MakeGenericMethod(entityType.ClrType);
+
+                method.Invoke(null, [modelBuilder]);
+            }
+
+            foreach (var property in entityType.GetProperties()
+                         .Where(p => p.ClrType == typeof(DateTime) || p.ClrType == typeof(DateTime?)))
+            {
+                property.SetValueConverter(new ValueConverter<DateTime, DateTime>(
+                    v => DateTime.SpecifyKind(v, DateTimeKind.Utc),
+                    v => DateTime.SpecifyKind(v, DateTimeKind.Utc)));
+            }
+        }
+    }
+
+    private static void SetSoftDeleteFilter<TEntity>(ModelBuilder modelBuilder) where TEntity : BaseEntity
+    {
+        modelBuilder.Entity<TEntity>().HasQueryFilter(e => !e.IsDelete);
+    }
+}
