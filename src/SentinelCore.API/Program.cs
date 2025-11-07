240b1cf7b291f404b756839ea8c21f0012dfe30b
Mohammad Hossein <hossinarab727@gmail.com>
2025-11-07 16:24:44 +0330

    Add Database Config

--- src/SentinelCore.API/Program.cs
+++ src/SentinelCore.API/Program.cs
@@ -1,9 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using SentinelCore.Infrastructure.Seed;
+using SentinelCore.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+    DbInitializer.Seed(db);
 }
 
 app.UseHttpsRedirection();
