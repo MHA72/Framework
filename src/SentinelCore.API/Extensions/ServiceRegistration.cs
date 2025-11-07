90ad08b841cb7642031e2fb8334c2044b995f439
Mohammad Hossein <hossinarab727@gmail.com>
2025-11-07 16:25:07 +0330

    Add ServiceRegistration

---
+++ src/SentinelCore.API/Extensions/ServiceRegistration.cs
@@ -0,0 +1,16 @@
+namespace SentinelCore.API.Extensions;
+
+public static class ServiceRegistration
+{
+    public static void AddApplicationServices(this IServiceCollection services)
+    {
+        // services.AddScoped<ISmsService, SmsService>();
+        // services.AddScoped<ILogService, LogService>();
+        // services.AddScoped<IUserService, UserService>();
+        // services.AddScoped<IAuthService, AuthService>();
+        // services.AddScoped<IRoleService, RoleService>();
+        // services.AddScoped<IAuditService, AuditService>();
+        // services.AddScoped<IPermissionService, PermissionService>();
+        // services.AddScoped<IRefreshTokenService, RefreshTokenService>();
+    }
+}
